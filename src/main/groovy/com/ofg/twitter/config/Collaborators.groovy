3c5ac977d588b5152eb1079b1a662390e0ca16f4
Maciej Kucharek <M.Kucharek@tt.com.pl>
2015-03-21 14:25:55 +0100

    Added missing collabolators

--- src/main/groovy/com/ofg/twitter/config/Collaborators.groovy
+++ src/main/groovy/com/ofg/twitter/config/Collaborators.groovy
@@ -7,5 +7,7 @@ class Collaborators {
 
     public static final String CLIENT_SERVICE_DEPENDENCY_NAME = 'client-service'
     public static final String LOAN_APPLICATION_SERVICE_DEPENDENCY_NAME = "loan-application-service"
+    public static final String DECISION_MAKER_SERVICE_DEPENDENCY_NAME = "decision-maker-service"
+    public static final String MARKETING_OFFER_GENERATOR_SERVICE_DEPENDENCY_NAME = "marketing-offer-generator-service"
 
 }
