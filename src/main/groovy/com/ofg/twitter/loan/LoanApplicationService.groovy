34e202c4cadcb0d1908af4eca4ed194626965347
Maciej Kucharek <M.Kucharek@tt.com.pl>
2015-03-21 14:02:21 +0100

    Added naive missing collaborators handlin

--- src/main/groovy/com/ofg/twitter/loan/LoanApplicationService.groovy
+++ src/main/groovy/com/ofg/twitter/loan/LoanApplicationService.groovy
@@ -1,35 +1,63 @@
 package com.ofg.twitter.loan
 
+import com.netflix.hystrix.HystrixCommand
+import com.nurkiewicz.asyncretry.AsyncRetryExecutor
+import com.ofg.infrastructure.discovery.ServiceUnavailableException
 import com.ofg.infrastructure.web.resttemplate.fluent.ServiceRestClient
 import groovy.json.JsonBuilder
+import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 /**
  * Created by mkucharek.
  */
+@Slf4j
 @Service
 class LoanApplicationService {
 
     @Autowired
     ServiceRestClient serviceRestClient
 
-    void applyForLoan(Loan loan) {
-        serviceRestClient
-                .forService("loan-application-service")
-                .post()
-                .onUrl("/api/loanApplication")
-                .body(new JsonBuilder(loan).toString()).ignoringResponse()
+    @Autowired
+    AsyncRetryExecutor executor
 
-        // TODO
+    //TODO: how do we handle ServiceUnavailable
+
+    void applyForLoan(Loan loan) {
+
+        try {
+            serviceRestClient
+                    .forService("loan-application-service")
+                    .retryUsing(executor.withMaxRetries(3))
+                    .post()
+                    .withCircuitBreaker(HystrixCommand.Setter.withGroupKey({ 'sendingLoanDetails' }))
+                    .onUrl("/api/loanApplication")
+                    .body(new JsonBuilder(loan).toString())
+                    .andExecuteFor()
+                    .ignoringResponse()
+
+        } catch (ServiceUnavailableException e) {
+            log.info("Cannot connect to collabolator", e)
+        }
 
     }
 
     void sendClientDetails(Client client) {
-        serviceRestClient.forService("client-service")
-                .post().onUrl("/api/client")
-                .body(new JsonBuilder(client).toString())
-                .ignoringResponse()
+
+        try {
+            serviceRestClient.forService("client-service")
+                    .retryUsing(executor.withMaxRetries(3))
+                    .post()
+                    .withCircuitBreaker(HystrixCommand.Setter.withGroupKey({ 'sendingClientDetails' }))
+                    .onUrl("/api/client")
+                    .body(new JsonBuilder(client).toString())
+                    .ignoringResponse()
+
+        } catch (ServiceUnavailableException e) {
+            log.info("Cannot connect to collabolator", e)
+        }
+
 
     }
 
